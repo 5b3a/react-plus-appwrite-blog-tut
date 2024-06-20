41b910a116ccd2f674f909791a528e37e602063f
0x5b3a <101mailme@protonmail.com>
2024-06-20 21:08:51 +0530

    finished auth services

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 import config from '../config/config'
 import {Account,Client,ID} from 'appwrite'
 
@@ -11,7 +12,51 @@ export class AUTHSERVICE{
         this.setProjecy(config.appwriteProjectId);
         this.account = new Account(this.client)
     }
+
+    async createAccount({email,password,name}){
+            try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name)
+            if (userAccount) {
+                return this.login({email,password})
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({email,password}){
+        try {
+            
+            const accountStatus =  await this.account.createEmailPasswordSession(email,password)
+            if (!accountStatus) {
+                return null
+            } else {
+                return accountStatus
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+    }
 }
+
 
 const authService = new AUTHSERVICE();
 
